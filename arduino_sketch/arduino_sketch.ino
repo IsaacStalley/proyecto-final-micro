c8b99ccb1ce7543d33a1ad8af7d6000a37f5058d
Isaac Stalley <isaacstalley@gmail.com>
2023-11-13 21:00:14 -0600

    Added pre-processing functions
    1. Resize function
    2. Greyscale function
    3. Normalize function

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -1,7 +1,15 @@
 #include <minist-keras-model_inferencing.h>
 #include <Arduino_OV767X.h>
 
-unsigned short pixels[160 * 120]; // QCIF: 176x144 X 2 bytes per pixel (RGB565)
+#define ORIGINAL_WIDTH 160
+#define ORIGINAL_HEIGHT 120
+#define TARGET_WIDTH 28
+#define TARGET_HEIGHT 28
+
+unsigned short pixels[ORIGINAL_WIDTH * ORIGINAL_HEIGHT]; // QQVGA: 160x120 X 16 bytes per pixel (RGB565)
+unsigned short resized_pixels[TARGET_WIDTH*TARGET_HEIGHT];
+unsigned char greyscale_pixels[TARGET_WIDTH * TARGET_HEIGHT];
+float normalized_pixels[TARGET_WIDTH * TARGET_HEIGHT];
 
 void setup() {
   Serial.begin(9600);
@@ -28,30 +36,61 @@ void setup() {
   Serial.println();
 }
 
+void resizeImage(unsigned short *original, unsigned short *resized) {
+  float x_ratio = (float)ORIGINAL_WIDTH / TARGET_WIDTH;
+  float y_ratio = (float)ORIGINAL_HEIGHT / TARGET_HEIGHT;
+
+  for (int y = 0; y < TARGET_HEIGHT; y++) {
+    for (int x = 0; x < TARGET_WIDTH; x++) {
+      int px = (int)(x * x_ratio);
+      int py = (int)(y * y_ratio);
+
+      int originalIndex = (py * ORIGINAL_WIDTH) + px;
+      int resizedIndex = (y * TARGET_WIDTH) + x;
+
+      resized[resizedIndex] = original[originalIndex];
+    }
+  }
+}
+
+void rgb565ToGreyscale(const unsigned short *rgb565Image, unsigned char *greyscaleImage) {
+  for (int i = 0; i < ORIGINAL_WIDTH * ORIGINAL_HEIGHT; i++) {
+    unsigned short pixel = rgb565Image[i];
+
+    // Extract individual color components
+    unsigned short red = (pixel >> 11) & 0x1F;
+    unsigned short green = (pixel >> 5) & 0x3F;
+    unsigned short blue = pixel & 0x1F;
+
+    // Convert to greyscale using luminosity method
+    unsigned char greyscale = (unsigned char)(0.299 * red + 0.587 * green + 0.114 * blue);
+
+    greyscaleImage[i] = greyscale;
+  }
+}
+
+void normalizeImage(unsigned char *greyscaleImage, float *normalizedImage) {
+  for (int i = 0; i < TARGET_WIDTH * TARGET_HEIGHT; i++) {
+    normalizedImage[i] = greyscaleImage[i] / 255.0f;
+  }
+}
+
 void loop() {
   if (Serial.read() == 'c') {
     Serial.println("Reading frame");
     Serial.println();
     Camera.readFrame(pixels);
+    resizeImage(pixels, resized_pixels);
+    rgb565ToGreyscale(resized_pixels, greyscale_pixels);
+    normalizeImage(greyscale_pixels, normalized_pixels);
 
-    int numPixels = Camera.width() * Camera.height();
+    int numPixels = TARGET_WIDTH * TARGET_HEIGHT;
 
     for (int i = 0; i < numPixels; i++) {
-      unsigned short p = pixels[i];
+      float p = normalized_pixels[i];
 
-      if (p < 0x1000) {
-        Serial.print('0');
-      }
-
-      if (p < 0x0100) {
-        Serial.print('0');
-      }
-
-      if (p < 0x0010) {
-        Serial.print('0');
-      }
-
-      Serial.print(p, HEX);
+      Serial.print(p);
+      Serial.print('/');
     }
   }
 }
